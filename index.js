9412f6a0dd5c70f1cf0ea1d9702279a151324b6d
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-04-23 14:52:45 +0600

    port connected

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4xsc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+client.connect((err) => {
+  const productscollection = client.db("Grameenponno").collection("products");
+ 
+});
+
+app.listen(port);
